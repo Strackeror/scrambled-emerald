d2e92d474580144e0fc6244b9dabdbd39b0bf27f
prankster20 <pranav.karmarkar@gmail.com>
2025-07-30 13:27:44 -0400

    Reroll NPC implementation

--- include/script_pokemon_util.h
+++ include/script_pokemon_util.h
@@ -13,4 +13,12 @@ void Script_GetChosenMonDefensiveEVs(void);
 void Script_GetChosenMonOffensiveIVs(void);
 void Script_GetChosenMonDefensiveIVs(void);
 
+void Script_SetNature(void);
+void Script_SetTeraType(void);
+void Script_ToggleShiny(void);
+void Script_ToggleGender(void);
+void Script_SetPokeball(void);
+void Script_GetChosenMonLevel(void);
+void Script_RemoveChosenMon(void);
+
 #endif // GUARD_SCRIPT_POKEMON_UTIL_H
